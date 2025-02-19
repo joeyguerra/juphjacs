d3e172324a51f84bbde0768414bec41aec8fdae6
Joey Guerra <github@joeyguerra.com>
2025-02-19 08:08:18 -0600

    add sitemap

---
+++ pages/sitemap.mjs
@@ -0,0 +1,25 @@
+import { Page } from '../src/Page.mjs'
+import { TemplateLiteralRenderer } from '../src/TemplateLiteralRenderer.mjs'
+
+class SitemapPage extends Page {
+    constructor (rootFolder, filePath, template) {
+        super(rootFolder, filePath, template, new TemplateLiteralRenderer())
+        this.title = 'Site Map'
+        this.links = []
+    }
+
+    async get (req, res) {
+        await this.render()
+        res.setHeader('Content-Type', 'text/xml')
+        res.statusCode = 200
+        res.end(this.content)
+    }
+}
+
+export {
+    SitemapPage
+}
+
+export default async (rootFolder, filePath, template) => {
+    return new SitemapPage(rootFolder, filePath, template)
+}
---
+++ plugins/Sitemap.mjs
@@ -0,0 +1,26 @@
+
+import { EVENTS } from '../src/Page.mjs'
+
+const links = new Set()
+
+class Link {
+    constructor(url, title) {
+        this.url = url
+        this.title = title
+    }
+}
+
+export default async () => {
+    process.on(EVENTS.PRE_TEMPLATE_RENDER, async (filePath, page) => {
+        if (filePath.includes('/sitemap.xml')) {
+            page.links = Array.from(links)
+        }
+    })
+
+    process.on(EVENTS.TEMPLATE_RENDERED, async (filePath, page) => {
+        if (!filePath.includes('sitemap.xml')) {
+            const link = new Link(page.route.filePath, page.title)
+            links.add(link)
+        }
+    })
+}
